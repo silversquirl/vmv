d5321e92ce1a854d2951b0fe0e0bb7c3f8b59d13
mlugg <mlugg@mlugg.co.uk>
2018-04-10 01:24:59 +0100

    Introduced visualiser bar configuration

--- config.lua
+++ config.lua
@@ -4,30 +4,29 @@ function bar_vis(bars)
   p2 = {}
   p3 = {}
   p4 = {}
-  color = {}
-
-  color[1] = 1
-  color[2] = 0
-  color[3] = 1
 
   for i, m in ipairs(bars) do
-    p1[1] = (i - 1) * width - 1
+    p1[1] = (i - 1) * width - 1 + bars_config['padding'] / 2
     p1[2] = -m / vmv.bar_max
 
-    p2[1] = (i - 1) * width - 1
+    p2[1] = (i - 1) * width - 1 + bars_config['padding'] / 2
     p2[2] = m / vmv.bar_max
 
-    p3[1] = i * width - 1
+    p3[1] = i * width - 1 - bars_config['padding'] / 2
     p3[2] = m / vmv.bar_max
 
-    p4[1] = i * width - 1
+    p4[1] = i * width - 1 - bars_config['padding'] / 2
     p4[2] = -m / vmv.bar_max
 
-    vmv.draw.rectangle(p1, p2, p3, p4, color)
+    vmv.draw.rectangle(p1, p2, p3, p4, bars_config['color'])
   end
 end
+
+bars_config = {}
+bars_config['color'] = {1, 0, 0}
+bars_config['padding'] = 0.1
+
 config = {}
 config['visualiser'] = bar_vis
-
 
 return config
