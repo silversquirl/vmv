75b0c0ffc862d553a89c80abe1629de16ff207b3
Samadi van Koten <samadi@vktec.co.uk>
2018-04-11 11:34:34 +0100

    Rewrite Lua to be more idiomatic
    - Use inline initialisation for tables (`table = { foo = bar, ... }` rather
      than `table = {}; table['foo'] = bar; ...`)
    - Use closures for configuring the visualiser rather than global variables

--- config.lua
+++ config.lua
@@ -7,37 +7,33 @@ This file allows you to specify which visualiser to use as well as various other
 ]]--
 
 -- Some custom config options provided by the sample visualiser, in vis.lua
-bars_config = {}
-bars_config['padding'] = 0.1
-bars_config['color'] = {1, 0, 1}
-bars_config['color_alt'] = {1, 0, 0}
+return {
+  -- This option should specify a function to use as a visualiser.
+  -- See vis.lua for more details.
+  visualiser = require("vis")({
+    padding = 0.1,
+    color = {1, 0, 1},
+    color_alt = {1, 0, 0},
+  }),
 
+  -- This option sets a key which can be used to close the window.
+  close_key = 'x',
 
-config = {}
+  -- This options sets the monitor on which the visualiser will appear.
+  monitor = 0,
 
--- This option should specify a function to use as a visualiser.
--- See vis.lua for more details.
-config['visualiser'] = require("vis")
+  -- This option sets the width of the visualiser window when it opens.
+  width = 512,
 
--- This option sets a key which can be used to close the window.
-config['close_key'] = 'x'
+  -- This option sets the height of the visualiser window when it opens.
+  height = 256,
 
--- This options sets the monitor on which the visualiser will appear.
-config['monitor'] = 0
+  -- This option sets the x position of the visualiser window when it opens.
+  x = 50,
 
--- This option sets the width of the visualiser window when it opens.
-config['width'] = 512
+  -- This option sets the y position of the visualiser window when it opens.
+  y = 50,
 
--- This option sets the height of the visualiser window when it opens.
-config['height'] = 256
-
--- This option sets the x position of the visualiser window when it opens.
-config['x'] = 50
-
--- This option sets the y position of the visualiser window when it opens.
-config['y'] = 50
-
--- If set to true, the visualiser window will always appear above all others.
-config['always_on_top'] = true
-
-return config
+  -- If set to true, the visualiser window will always appear above all others.
+  always_on_top = true,
+}
--- vis.lua
+++ vis.lua
@@ -1,30 +1,30 @@
-function bar_vis(bars)
-  table.remove(bars, 1)
-  width = 2 / #bars
-  p1 = {}
-  p2 = {}
-  p3 = {}
-  p4 = {}
+return function (config)
+  return function (bars)
+    table.remove(bars, 1)
+    local width = 2 / #bars
+    local p1 = {}
+    local p2 = {}
+    local p3 = {}
+    local p4 = {}
 
-  for i, m in ipairs(bars) do
-    p1[1] = (i - 1) * width - 1 + bars_config['padding'] / 2
-    p1[2] = -m / vmv.bar_max
+    for i, m in ipairs(bars) do
+      p1[1] = (i - 1) * width - 1 + config['padding'] / 2
+      p1[2] = -m / vmv.bar_max
 
-    p2[1] = (i - 1) * width - 1 + bars_config['padding'] / 2
-    p2[2] = m / vmv.bar_max
+      p2[1] = (i - 1) * width - 1 + config['padding'] / 2
+      p2[2] = m / vmv.bar_max
 
-    p3[1] = i * width - 1 - bars_config['padding'] / 2
-    p3[2] = m / vmv.bar_max
+      p3[1] = i * width - 1 - config['padding'] / 2
+      p3[2] = m / vmv.bar_max
 
-    p4[1] = i * width - 1 - bars_config['padding'] / 2
-    p4[2] = -m / vmv.bar_max
+      p4[1] = i * width - 1 - config['padding'] / 2
+      p4[2] = -m / vmv.bar_max
 
-    if i % 2 == 0 then
-      vmv.draw.rectangle(p1, p2, p3, p4, bars_config['color'])
-    else
-      vmv.draw.rectangle(p1, p2, p3, p4, bars_config['color_alt'])
+      if i % 2 == 0 then
+        vmv.draw.rectangle(p1, p2, p3, p4, config['color'])
+      else
+        vmv.draw.rectangle(p1, p2, p3, p4, config['color_alt'])
+      end
     end
   end
 end
-
-return bar_vis
